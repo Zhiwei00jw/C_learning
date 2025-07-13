c7b252a9adf93e553c0b322e5418f2e1ad2c248c
知微 <19883129660@163.com>
2025-07-13 16:13:36 +0800

    版本2：优化输出形式—每行数字右对齐，且每5个换一行

--- src/04_data_structures/Fibonacci_Sequence.c
+++ src/04_data_structures/Fibonacci_Sequence.c
@@ -1,18 +1,24 @@
 /*计算斐波那契数列前46个数，并按照每行5个的格式输出*/
 #include <stdio.h>
-
+#define MAXN 46 //宏定义定义符号常量，值为46，后续如果想要输出其他的值，可以直接修改宏定义；
 int main(){
-    int fib[46]={1,1};
+    int fib[MAXN]={1,1};
     int i,j;
     i=2;
-    for(i;i>=2&&i<=45;i++){
+    for(i;i>=2&&i<MAXN;i++){
 
         fib[i]=fib[i-1]+fib[i-2];
     }
-    for(j=0;j<=45;j++){
-        printf("%d\n",fib[j]);
-
+    //输出斐波那契数列，实现功能，每输出5个就换行
+    for(j=0;j<MAXN;j++){
+        printf("%11d",fib[j]);
+        if((j+1)%5==0){
+            printf("\n"); //换行
+        }
+            
     }
+        if(MAXN%5!=0)
+            printf("\n");
     system("pause");  // 会显示 "Press any key to continue..."
     return 0;
 
