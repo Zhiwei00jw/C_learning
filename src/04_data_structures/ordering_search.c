42b04230157e1a129beefaef3d6990574c6307d8
知微 <19883129660@163.com>
2025-07-14 11:05:36 +0800

    version 2 :增加了查找最大下标和最小下标的功能

--- src/04_data_structures/ordering_search.c
+++ src/04_data_structures/ordering_search.c
@@ -1,11 +1,14 @@
-/*使用顺序查找法逐个查找数组中是否有相应元素*/
+/*使用顺序查找法逐个查找数组中是否有相应元素，并输出相应下标的最大值和最小值*/
 #include <stdio.h>
 #define MAXN 10
 int main(){
-    int a[MAXN];
+    int a[MAXN]={0};
+    int b[MAXN]={0};
     int n=0,x=0;
     int num=0;
-    int i=0;
+    int i=0,j=0;
+    int maxm=0;
+    int min=0;
     printf("Enter n,x:");
     scanf("%d%d",&n,&x);
     printf("Enter %d integers",n);
@@ -15,12 +18,28 @@ int main(){
     for(i=0;i<n;i++){
         if(a[i]==x){
             num++;
-            printf("index=%d\n",i);
+            b[j]=i;
+            j++;
         }
 
+    }//判断数组中是否有数与x相同
+    if(num==0){
+        printf("NOT FOUND");   
+    }else{
+        min=num;//遍历求得下标的最大值和最小值
+        for(j=0;j<num;j++){
+            printf("b[%d]=%d\n",j,b[j]);
+            if(maxm<b[j])
+                maxm=b[j];
+            if(min>b[j])
+                min=b[j];
+        }
+        printf("the maxm index is %d\n",maxm);
+        printf("the minest index is %d\n",min);
+       
+
     }
-    if(num==0)
-    printf("NOT FOUND");
+    
     system("pause"); 
     return 0;
 
