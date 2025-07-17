154e95f9a575236739c0e3fa5ce51669520561dc
知微 <19883129660@163.com>
2025-07-17 16:19:37 +0800

    顺序输入二维数组中的数据

---
+++ src/05_arrays/07_2_two_dimension_arrays_exercises/input_matrix.c
@@ -0,0 +1,27 @@
+/*借助二维数组，输入矩阵*/
+#include <stdio.h>
+#define MAXM 6
+#define MAXN 6
+int main(){
+    int i=0,j=0,n=0,m=0;
+    int a[MAXM][MAXN];
+    printf("Enter n:");
+    scanf("%d",&n);
+    printf("Enter m:");
+    scanf("%d",&m);
+    //输入nxm个数据存入数组
+    printf("Enter %dx%d intergers",n,m);
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            printf("a[%d][%d]=%d\n",i,j,a[i][j]);
+        }
+    }
+    system("pause");
+    return 0;
+    
+}
