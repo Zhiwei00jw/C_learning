cc97e2472bed333cfafd5f883811ecce6b04c18e
知微 <19883129660@163.com>
2025-07-17 16:33:55 +0800

    切换输入循环中外循环i和里循环的次序

--- src/05_arrays/07_2_two_dimension_arrays_exercises/input_matrix.c
+++ src/05_arrays/07_2_two_dimension_arrays_exercises/input_matrix.c
@@ -11,11 +11,20 @@ int main(){
     scanf("%d",&m);
     //输入nxm个数据存入数组
     printf("Enter %dx%d intergers",n,m);
+    /*
     for(i=0;i<n;i++){
         for(j=0;j<m;j++){
             scanf("%d",&a[i][j]);
         }
     }
+        */
+    //输入矩阵，按照先列后行的顺序
+    for(j=0;j<m;j++){
+        for(i=0;i<n;i++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    //输出矩阵，按照正常顺序
     for(i=0;i<n;i++){
         for(j=0;j<m;j++){
             printf("a[%d][%d]=%d\n",i,j,a[i][j]);
