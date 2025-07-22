e1a6be3e066ff2defc60e65c55ae5493e0eb7b6e
知微 <19883129660@163.com>
2025-07-22 17:55:20 +0800

    凯撒密码解密

---
+++ src/05_arrays/07_3_character_string/caesar_decrypt.c
@@ -0,0 +1,46 @@
+//对字符串进行凯撒密码解密操作
+#include <stdio.h>
+#define MAXLINE 80
+#define M 26
+int main(){
+    int i=0;
+    char s[MAXLINE];
+    int offset=0;
+    int k=0;
+    //输入字符串数组和偏移量
+    printf("Enter a string:");
+    while((s[k]=getchar())!='\n'){
+        k++;
+    }
+    s[k]='\0';
+    printf("Enter offset:");
+    scanf("%d",&offset);
+    //对密码进行解密
+    while(s[i]!='\0'){
+        if(s[i]>='A'&&s[i]<='Z'){
+            if(s[i]-'A'-offset>=0){
+                s[i]=s[i]-offset;
+            }else{
+                s[i]=s[i]+(M-offset);
+            }
+        }else if(s[i]>='a'&&s[i]<='z'){
+                if(s[i]-'a'-offset>=0){
+                    s[i]=s[i]-offset;
+                }else{
+                    s[i]=s[i]+(M-offset);
+                }
+        }
+        i++;                
+    }
+    //输出加密后的密码
+    printf("After being encrypted:");
+    i=0;
+    while(s[i]!='\0'){
+        putchar(s[i]);
+        i++;
+    }
+    printf("\n");
+    system("pause");
+    return 0;
+
+}
