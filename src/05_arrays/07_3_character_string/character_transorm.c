f61ab424734ace4917241e7e5a11a327e226ecbd
知微 <19883129660@163.com>
2025-07-22 17:09:35 +0800

    feat:version2:优化生成十进制数字的代码片段

--- src/05_arrays/07_3_character_string/character_transorm.c
+++ src/05_arrays/07_3_character_string/character_transorm.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 #define MAXLINE 10
 int main(){
-    int i=0,k=0,j=0;
+    int i=0,k=0;
     int sum=0;
     char s[MAXLINE];
-    int num[MAXLINE];
     //输入数组
     printf("Enter a string:");
     while((s[k]=getchar())!='\n'){
@@ -15,16 +14,9 @@ int main(){
     //遍历数组，将字符串中的数字存储到另一个数组中
     for(i=0;i<k;i++){
         if(s[i]>='0'&&s[i]<='9'){
-            num[j]=s[i]-'0';
-            j++;
+            sum=sum*10+(s[i]-'0');//这行很关键，将字符'4'转换成十进制数字 4
         }
     }
-    //将数组中的数生成为一个数字
-    for(i=0;i<j;i++){
-        sum+=num[i];
-        sum*=10;
-    }
-    sum=sum/10;
     printf("The number is %d\n",sum);
     system("pause");
     return 0;
