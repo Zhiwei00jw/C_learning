bb6d3027195bdbf70ee412baa96a08295e58ea51
知微 <19883129660@163.com>
2025-07-26 11:02:39 +0800

    feat:用指针计算数组元素的和

--- src/08_pointer/array_pointer.c
+++ src/08_pointer/array_pointer.c
@@ -7,6 +7,7 @@ int main(){
     int sum = 0;
     int i;
     int a[MAX];
+    int *p = a; // 指针p指向数组a的首元素
     printf("Enter n:");
     scanf("%d",&n);
     printf("Enter %d integers:",n);
@@ -17,6 +18,14 @@ int main(){
     for(i=0;i<n;i++){
         printf("%d\n ",a[i]);
     }
+    //另一种使用指针计算数组元素和的方法
+    /*
+    sum = 0; // 初始化sum为0
+    for(p=a; p < a + n; p++){ // p指向数组的首元素，直到数组末尾
+        sum += *p; // 累加指针p所指向的值
+    }   
+    */
+    
     printf("Sum = %d\n",sum);
     system("pause");
     return 0;
@@ -25,7 +34,7 @@ int main(){
 void sum_(int *array,int *sum,int n){
     int i;
     *sum = 0; // 初始化sum为0
-    for(i=0;i<n;i++){
+    for(i=0;i<n;i++){   //这里用了一个辅助变量i 去控制循环次数
         *sum+=*(array++);
     }
 }
