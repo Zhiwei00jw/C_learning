232be5a5c69495961a6c0bbf77e223445eea63fa
知微 <19883129660@163.com>
2025-09-01 22:17:12 +0800

    feat:使用指针在各函数间传递数组大小count值;input_array中删除数组重复怨妇更改为双指针法

--- src/10_functions/functions/ordered_sheet.c
+++ src/10_functions/functions/ordered_sheet.c
@@ -6,10 +6,10 @@
 //不使用全局变量count；int count=0;
 // 函数声明
 int input_array(int *a);
-void select(int *a,int operation,int value,int count);
-void insert(int *a,int value,int count);
-void remove_value(int *a,int value,int count);
-int query(int *a,int value,int count);
+void select(int *a,int operation,int value,int *count);
+void insert(int *a,int value,int *count);
+void remove_value(int *a,int value,int  *count);
+int query(int *a,int value,int *count);
 void print_array(int *a,int num);
 int main() {
     int a[MAX];
@@ -26,7 +26,7 @@ int main() {
     scanf("%d",&operation);
     printf("Enter value:\n");
     scanf("%d",&value);
-    select(a,operation,value,count);
+    select(a,operation,value,&count);
     system("pause");
     return 0;
 }
@@ -36,6 +36,7 @@ int input_array(int *a) {
     int i=0;
     int count=0;
     int min,j,index,delete=0,temp;
+    int *p_array1=&a[0],*p_array2=p_array1+1;
     //输入数组中数据个数，修改全局变量count以便于各个函数共用
     printf("Enter count:\n");
     scanf("%d",&count);
@@ -55,12 +56,14 @@ int input_array(int *a) {
                 index=i;
             }
         }
+        //将数组中的最小数挪动到最前面位置，然后从数组的第二个数开始继续寻找最小值，以此类推
         temp=a[index];
         a[index]=a[j];
         a[j]=temp;
         min=a[j+1];
     }
-    //排好序的数组删除重复数字
+    /*
+    //排好序的数组删除重复数字，遍历数组，发现重复数字时就向前挪动一位，移动效率低
     i=1;
     while(i<count-delete){
         if(a[i]==a[i-1]){
@@ -74,10 +77,26 @@ int input_array(int *a) {
             i++;
         }
     }
+    */
+   //使用双指针删除重复元素
+   index=0;
+   while(index<count-1){    //count-1是指针2移动次数
+        if(*p_array1==*p_array2){
+            p_array2++;
+            index++;
+            delete++;
+        }else{
+            p_array1++;
+            *(p_array1)=*p_array2;
+            p_array2++;
+            index++;
+        }
+   }
+
     print_array(a,count-delete);
     return count-delete;
 }
-void select(int *a,int operation,int value,int count){
+void select(int *a,int operation,int value,int *count){
     switch (operation)
     {
     case 1:
@@ -95,49 +114,58 @@ void select(int *a,int operation,int value,int count){
     }
 
 }
-void insert(int *a,int value,int count){
-    int i=0,j=0;
+void insert(int *a,int value,int *count){
+    int i=0,j=0,index=1;
     //从a[i]开始逐个往后移一位
-    for(i=0;i<count;i++){
+    for(i=0;i<*count;i++){
         if(value<=a[i]){
             if(value<a[i]){
-                for(j=count;j>i;j--){
+                index=0;
+                for(j=*count;j>i;j--){
                     a[j]=a[j-1];
                 }
                 a[j]=value;
-            }else{
+            
+            }else{  //重复数字不插入
                 printf("Repeated number.\n");
-                count--;
             }
 
             break;
         }
     }
-    //如果把value插入到数组最后
-    if(i>=count){
-        a[count]=value;
+    //插入后数组的值加一
+    if(index==0){
+        (*count)++;
     }
-    print_array(a,count+1);
+    //value值大于数组中所有数字，则插入到最后
+    if(i>=*count){
+        a[*count]=value;
+        (*count)++;
+
+    }
+    print_array(a,*count);
 
 }
-void remove_value(int *a,int value,int count){
+void remove_value(int *a,int value,int *count){
     //调用查找函数直接返回待查找值的位置
     int index=query(a,value,count);
     //没找到，直接打印原数组
     if(index==-1){
-        print_array(a,count);
+        print_array(a,*count);
     }else{          //找到了，从a[i]开始逐个往前移一位
         int i;
-        for(i=index;i<count-1;i++){
+        for(i=index;i<*count-1;i++){
             a[i]=a[i+1];
         }
-        print_array(a,count-1);   
+        (*count)--;
+        print_array(a,*count);   
     }
 }
-int query(int *a,int value,int count){
+int query(int *a,int value,int *count){
     int low=0;
-    int high=count-1;
+    int high=*count-1;
     int mid,index=0;
+    //二分查找法
     //循环判断是否存在待查找数，退出条件是low>high
     while(low<=high){
         mid=(low+high)/2;
